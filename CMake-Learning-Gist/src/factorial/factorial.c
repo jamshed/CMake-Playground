3694b827d68a31e680ddc39c2db26e3b456e6d5c
jamshed <jamshed@umd.edu>
2020-05-05 03:02:51 -0400

    Add compile warning options in gist example

--- CMake-Learning-Gist/src/factorial/factorial.c
+++ CMake-Learning-Gist/src/factorial/factorial.c
@@ -9,5 +9,8 @@ uint64_t factorial(int num)
     if(num == 0)
         return 1;
 
+    int f = num;    // While building the project, check how the `make` or the `make install` produces a warning
+                    // for this line, as the warning flags are enabled in the CMake options.
+
     return num * factorial(num - 1);
 }
--- CMake-Learning-Gist/src/main.cpp
+++ CMake-Learning-Gist/src/main.cpp
@@ -17,5 +17,8 @@ int main(int argc, char** argv)
     greet_user(argv[1]);
     printf("The factorial of %s is %lu.\n", argv[2], factorial(atoi(argv[2])));
 
+    7;  // While building the project, check how the `make` or the `make install` produces a warning
+        // for this line, as the warning flags are enabled in the CMake options.
+
     return EXIT_SUCCESS;
 }
